4b80befbcbc80742534d9cbcd0445ed5d0b3c7dd
Jake Helme <jake.helme@gmail.com>
2019-12-23 22:05:28 +0200

    day8 - two correct

--- day8/Picture.js
+++ day8/Picture.js
@@ -46,4 +46,31 @@ Picture.prototype.checkSum = function() {
 	return numberOfOnes * numberOfTwos;
 }
 
+Picture.prototype.render = function() {
+	const renderedOutput = [];
+	this.layers[0].forEach((_, i) => {
+		renderedOutput.push(this.getPixelColour(i));
+	});
+	for (let i = 0; i < this.height; i++) {
+		let row = '';
+		for (let j = 0; j < this.width; j++) {
+			row = `${row}${renderedOutput[(i * this.width) + j] === 1 ? '\x1b[47m \x1b[0m' : '\x1b[40m \x1b[0m'}`;
+		}
+		console.log(row);
+	}
+}
+
+Picture.prototype.getPixelColour = function(pixelIndex) {
+	let currentLayer = 0;
+	while(currentLayer !== this.layers.length) {
+		const currentPixel = this.layers[currentLayer][pixelIndex];
+		if( currentPixel !== 2) {
+			return currentPixel;
+		} else {
+			currentLayer++;
+		}
+	}
+	return 2;
+}
+
 module.exports = Picture;
---
+++ day8/two.js
@@ -0,0 +1,5 @@
+const Picture = require('./Picture');
+const pixels = require('./input');
+
+const passwordPic = new Picture(25, 6, pixels);
+passwordPic.render();
